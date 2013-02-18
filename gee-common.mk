60303106c198780b225c91eace23299c4a841c5e
synergy dev <synergye@codefi.re>
2013-02-17 17:21:35 -0800

    fix ramdisk paths

--- gee-common.mk
+++ gee-common.mk
@@ -44,8 +44,8 @@ PRODUCT_PACKAGES += \
 
 # Ramdisk
 PRODUCT_COPY_FILES += \
-        device/lge/gee-common/init.gee.rc:root/init.gee.rc \
-        device/lge/gee-common/init.gee.usb.rc:root/init.gee.usb.rc
+        device/lge/gee-common/ramdisk/init.gee.rc:root/init.gee.rc \
+        device/lge/gee-common/ramdisk/init.gee.usb.rc:root/init.gee.usb.rc
 
 PRODUCT_COPY_FILES += \
 	device/lge/gee-common/WCNSS_cfg.dat:system/vendor/firmware/wlan/prima/WCNSS_cfg.dat \
