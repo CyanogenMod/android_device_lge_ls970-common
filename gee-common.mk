7a93e18003cd04178c0662638d11376b417bb84d
Seth Shelnutt <shelnutt2@gmail.com>
2013-03-12 20:06:40 -0400

    Add common rild.libpath back in.
    Change-Id: I6a4e63bb4eb4c48100c625fc3b876d0bc26921c5

--- gee-common.mk
+++ gee-common.mk
@@ -159,6 +159,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.telephony.call_ring.multiple=0
 
+# QC RIL path for rild
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    rild.libpath=/system/lib/libril-qc-qmi-1.so 
+
 #Upto 3 layers can go through overlays
 PRODUCT_PROPERTY_OVERRIDES += persist.hwc.mdpcomp.enable=true
 
