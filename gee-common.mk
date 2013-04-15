73d279989f6dcd4909e54276a6073d2513093f6b
Shawn Alty <shawn.alty@gmail.com>
2013-04-14 22:57:13 -0500

    Remove property override
    Replaced with script for actual name resolution
    
    Conflicts:
    	gee-common.mk
    
    Change-Id: Ia33f96b264d7d5b5fbf10363bf597c73090db057

--- gee-common.mk
+++ gee-common.mk
@@ -142,6 +142,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml
 
+# Script for baseband name resolution
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/fetch-swv:system/bin/fetch-swv
+
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.opengles.version=131072
 
